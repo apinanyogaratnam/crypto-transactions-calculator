79fc4db4cd2107e740d9628e4671549ddd313449
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2022-01-25 18:57:24 -0500

    refactor: reading shakepay data

--- calculate.py
+++ calculate.py
@@ -6,3 +6,11 @@ with open('newton.csv') as f:
 
     for line in f:
         data.append(line)
+
+columns = None
+data = []
+with open('shakepay.csv') as f:
+    columns = f.readline().strip().split(',')
+
+    for line in f:
+        data.append(line)
